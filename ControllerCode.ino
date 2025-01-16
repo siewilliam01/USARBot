5f393d657f37d025dbed9afcebd10d0e33006b19
siewilliam01 <siewilliam01@gmail.com>
2025-01-15 21:45:50 -0800

    Update ControllerCode.ino
    reorganize functions and locations of parts of code

--- ControllerCode.ino
+++ ControllerCode.ino
@@ -21,8 +21,8 @@ unsigned long txIntervalMillis = 1000; // send once per second
 
 int X = 0;
 int Y = 0;
-String comma;
-String space;
+String comma = ", ";
+String space = "  ";
 int leftMotor;
 int rightMotor;
 int XClaw = 0;
@@ -47,33 +47,18 @@ void setup()
 
 void loop() 
 {
-  currentMillis = millis();
-  X = map(analogRead(VRX), 0, 1023, 0, 180);
-  Y = map(analogRead(VRY), 0, 1023, 0, 180);
-  comma = ",";
-  space = "  ";
-  XClaw = map(analogRead(VRXClaw), 0, 1023, 800, 2200);
-  YArm = map(analogRead(VRYArm), 0, 1023, 55, 125);
-  XRotate = map(analogRead(VRXRotate), 0, 1023, 70, 120);
-  YArmBottom = map(analogRead(VRYArmBottom), 0, 1023, 45, 135);
-
-  //if(currentMillis - prevMillis >= txIntervalMillis) //freezes the output when numbers is too high or low for some reason
-  //{
-    send();
-    //prevMillis = millis();
-  //}
+  movement();
+  claw();
+  arm();
+  rotate();
+  armBottom();
+  send();
   delay(50);
 }
 
 
 void send() 
 {
-  //movement();
-  movement2();
-  claw();
-  arm();
-  rotate();
-  armBottom();
   int sentData[6];
   sentData[0] = leftMotor;
   sentData[1] = rightMotor;
@@ -85,7 +70,7 @@ void send()
   bool rslt;
   rslt = radio.write( &sentData, sizeof(sentData) );
   //Serial.print(leftMotor + comma + rightMotor + comma + clawServo + comma + armMotor + comma + rotateMotor + comma + armBottomMotor + space);
-  Serial.print(sentData[0] + comma + sentData[1] + comma + sentData[2] + comma + sentData[3] + comma + sentData[4] + comma + sentData[5] + space);
+  Serial.print(sentData[0] + comma + sentData[1] + comma + sentData[2] + comma + sentData[3] + comma + sentData[4] + comma + sentData[5] + space + space + space);
   Serial.print(X + comma + Y + space);
   Serial.print(XRotate + comma + YArmBottom + space);
   Serial.print(XClaw + comma + YArm + space);
@@ -100,48 +85,11 @@ void send()
   }
 }
 
-void movement() //left and right motor evaluations/calcs
+void movement() //left and right motor evaluations/calcs, DL and DR
 {
-  leftMotor = 90;
-  rightMotor = 90;
-  if((Y>95 || Y<85) && X<=95 && X>=85)
-  {
-    leftMotor = Y;
-    rightMotor = Y;
-    if(Y>95) //just to make it clearer for me which direction it is indicating
-    {
-      Serial.print("↑ ");
-    }
-    else if (Y<85)
-    {
-      Serial.print("↓ ");
-    }
-  }
-  if(Y<=95 && Y>=85)
-  {
-    if(X<85)
-    {
-      leftMotor = X;
-      rightMotor = map(X,90,0,90,180);
-      Serial.print("← ");
-    }
-    else if(X>95)
-    {
-      leftMotor = X;
-      rightMotor = map(X,90,180,90,0);
-      Serial.print("→ ");
-    }
-  }
-}
-
-void movement2() //left and right motor evaluations/calcs
-{
-  leftMotor = 90;
-  rightMotor = 90;
+  X = map(analogRead(VRX), 0, 1023, -90, 90);
+  Y = map(analogRead(VRY), 0, 1023, -90, 90);
   
-  Y = map(Y, 0, 180, -90, 90);
-  X = map(X, 0, 180, -90, 90);
-
   leftMotor = Y - X;
   rightMotor = Y + X;
 
@@ -169,8 +117,9 @@ void movement2() //left and right motor evaluations/calcs
   }
 }
 
-void claw()
+void claw() //AG
 {
+  XClaw = map(analogRead(VRXClaw), 0, 1023, 800, 2200);
   clawServo = 0;
   if(!(XClaw > 1480 && XClaw < 1510))
   {
@@ -180,8 +129,9 @@ void claw()
 }
 
 
-void arm() //this does nothing
+void arm() //A3
 {
+  YArm = map(analogRead(VRYArm), 0, 1023, 55, 125);
   armMotor = YArm;
   if(armMotor <= 89 && armMotor >= 87)
   {
@@ -189,8 +139,9 @@ void arm() //this does nothing
   }
 }
 
-void rotate()
+void rotate() //A1
 {
+  XRotate = map(analogRead(VRXRotate), 0, 1023, 70, 120);
   rotateMotor = XRotate;
   if(XRotate==89)
   {
@@ -198,8 +149,9 @@ void rotate()
   }
 }
 
-void armBottom()
+void armBottom() //A2
 {
+  YArmBottom = map(analogRead(VRYArmBottom), 0, 1023, 45, 135);
   armBottomMotor = YArmBottom;
   if(YArmBottom > 86 && YArmBottom < 90)
   {
